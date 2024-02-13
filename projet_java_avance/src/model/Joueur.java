e8d855e83e75fc38f47af624d576c728c28359ac
Antony Dias <antony.overwatch@gmail.com>
2024-02-13 16:38:54 +0100

    mise en place de la classe joueur

--- projet_java_avance/src/model/Joueur.java
+++ projet_java_avance/src/model/Joueur.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Joueur {
+
+    public String pseudo;
+    public int[] position;
+    public boolean mort;
+
+    public static void bouger(){
+
+    }
+
+    public static void détruire(){
+
+    }
+
 }

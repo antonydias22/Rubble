b21cde910c4f7ad2ecfc6102db30cd0a30e7161c
tenneguin nicolas <nicolas.tenneguin@gmail.com>
2024-02-14 16:48:16 +0100

    ajout de la branche main

--- projet_java_avance/src/Main.java
+++ projet_java_avance/src/Main.java
@@ -1,21 +1,20 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import model.*;
+import java.util.ArrayList;
+
 public class Main {
 
 
 
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Jeu test = new Jeu();
+        ArrayList<Joueur> liste = new ArrayList<Joueur>();
+        int[][] tab = new int [12][132];
+        Jeu.initialisation_jeu(tab, liste);
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
     }
+
 }

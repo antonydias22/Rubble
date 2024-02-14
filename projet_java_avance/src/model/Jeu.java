85d4ab043b2f72510afef8f6581cb172b6b0c858
tenneguin nicolas <nicolas.tenneguin@gmail.com>
2024-02-14 10:31:49 +0100

    ajout de la fonction start pour init la partie

--- projet_java_avance/src/model/Jeu.java
+++ projet_java_avance/src/model/Jeu.java
@@ -1,6 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Jeu {
+
+    /**
+     * Fonction permettant d'afficher le plateau de jeu lorsque l'on lui demande, elle va alors afficher toutes les
+     * valeurs d'un tableau à l'execption des extrémité
+     * @param tableau : le tableau qui contient les infos que l'on veut afficher
+     * @param axe_x : taille du plateau en hauteur
+     * @param axe_y : taille du plateau en largeur
+     * @param compte_x : pointeur selon l'axe horizontale qui va nous permettre de nous repérer sur le plateau
+     * @param compte_y : pointeur selon l'axe verticale qui va nous permettre de nous repérer sur le plateau
+     */
     public static void afficherTableau(int[][] tableau, int axe_x, int axe_y, int compte_x, int compte_y) {
 
         if ((compte_x == 1) && (compte_y == 1))
@@ -21,6 +34,12 @@ public class Jeu {
         }
     }
 
+    /**
+     *
+     * @param tableau: le tableau qui contient les infos que l'on veut afficher
+     * @param compte_x: pointeur selon l'axe horizontale qui va nous permettre de nous repérer sur le plateau
+     * @param compte_y: pointeur selon l'axe verticale qui va nous permettre de nous repérer sur le plateau
+     */
     public static void init_tableau(int[][] tableau, int compte_x, int compte_y)
     {
         if (((compte_y == 0)||(compte_y == 3)||(compte_x == 0)||(compte_x == 4)))
@@ -41,5 +60,46 @@ public class Jeu {
             tableau[compte_y][compte_x] = 0;
             init_tableau(tableau,compte_x+1,compte_y);
         }
+    }
+
+    /**
+     * Cette fonction permet d'initialiser la partie en donnant un nom au joueur, préparant le plateau et donnant le
+     * joueur qui va commencer la partie
+     * @param tableau: le tableau qui contient les infos que l'on veut afficher
+     * @param joueur: la liste des joueurs que l'on va remplir à l'initialisation de la partie
+     * @return on retourne le joueur qui va commencer pour lancer notre boucle de jeu
+     */
+    public static int initialisation_jeu(int[][]tableau, ArrayList<Joueur> joueur)
+    {
+        //demander nombre de joueur
+        int nb_joueur = 2;
+        //création des joueurs dans la liste joueur
+        for(int i = 0;i < nb_joueur;i++) {
+            Joueur objet = new Joueur();
+            objet.mort = false;
+            // en fonction du joueur, on va leur donner une position de départ différentes
+            if (i == 0)
+            {
+                objet.position[0] = 0;
+                objet.position[1] = 0;
+            }
+            else if(i == 1)
+            {
+                objet.position[0] = 1;
+                objet.position[1] = 1;
+            }
+            //on donne un pseudo différent de chaque joueur
+            objet.pseudo = "bloop" + i;
+            joueur.add(objet);
+        }
+
+        //remplissage variable quijoue avec un nombre aléatoire
+        int aqui;
+        Random r = new Random();
+        aqui = r.nextInt(nb_joueur);
+        //initialisation du plateau de jeu
+        Jeu.init_tableau(tableau,0,0);
+
+        return  aqui;
     }
 }

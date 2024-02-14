924fe0207fe235373953dec77b72ac44cd0081d4
Antony Dias <antony.overwatch@gmail.com>
2024-02-14 13:53:19 +0100

    amélioration de la fonction bouger

--- projet_java_avance/src/model/Joueur.java
+++ projet_java_avance/src/model/Joueur.java
@@ -8,7 +8,8 @@ public class Joueur {
     public int[] position;
     public boolean mort;
 
-    public void bouger() {
+    public void bouger(int[][] plateau) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Où voulez-vous vous déplacer ?");
         System.out.println("1• Haut\n2• Bas\n3• Gauche\n4• Droite");
@@ -22,33 +23,58 @@ public class Joueur {
             scanner.next();
             return;
         }
-        int newX = position[0], newY = position[1];
+        int newX = position[1], newY = position[0];
 
         switch (se_deplacer) {
             case 1:
-                newY -= 1;
+                if((plateau[newY-1][newX] == -1) || (plateau[newY-1][newX] == 1) || (plateau[newY-1][newX] == 2)
+                || (plateau[newY-1][newX] == 3) || (plateau[newY-1][newX] == 4)){
+                    System.out.println("Tu ne peux pas te déplacer ici !");
+                    bouger(plateau);
+                }
+                else{
+                    newY -= 1;
+                }
                 break;
             case 2:
-                newY += 1;
+                if((plateau[newY+1][newX] == -1) || (plateau[newY+1][newX] == 1) || (plateau[newY+1][newX] == 2)
+                        || (plateau[newY+1][newX] == 3) || (plateau[newY+1][newX] == 4)){
+                    System.out.println("Tu ne peux pas te déplacer ici !");
+                    bouger(plateau);
+                }
+                else {
+                    newY += 1;
+                }
                 break;
             case 3:
-                newX -= 1;
+                if((plateau[newY][newX-1] == -1) || (plateau[newY][newX-1] == 1) || (plateau[newY][newX-1] == 2)
+                        || (plateau[newY][newX-1] == 3) || (plateau[newY][newX-1] == 4)){
+                    System.out.println("Tu ne peux pas te déplacer ici !");
+                    bouger(plateau);
+                }
+                else {
+                    newX -= 1;
+                }
                 break;
             case 4:
-                newX += 1;
+                if((plateau[newY][newX+1] == -1) || (plateau[newY][newX+1] == 1) || (plateau[newY][newX+1] == 2)
+                        || (plateau[newY][newX+1] == 3) || (plateau[newY][newX+1] == 4)){
+                    System.out.println("Tu ne peux pas te déplacer ici !");
+                    bouger(plateau);
+                }
+                else {
+                    newX += 1;
+                }
                 break;
             default:
                 System.out.println("Ce n'est pas une direction valide !");
                 return;
         }
 
-        if (estBloque(newX, newY)) {
-            System.out.println("Cette direction est bloquée !");
-        } else {
             position[0] = newX;
             position[1] = newY;
             System.out.println("Déplacement effectué !");
-        }
+
     }
 
     public void detruire(){
@@ -57,8 +83,7 @@ public class Joueur {
 
     public boolean estBloque(int x, int y){
 
-    return false;
-
+        
     }
 
 }

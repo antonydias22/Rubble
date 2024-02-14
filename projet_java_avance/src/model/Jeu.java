d0df5a21b9cb7ed420dbbbe8507d76da89ce717e
iblms <ilyas170605@gmail.com>
2024-02-14 14:58:28 +0100

    ajout des commentaires sur la class Jeu

--- projet_java_avance/src/model/Jeu.java
+++ projet_java_avance/src/model/Jeu.java
@@ -16,46 +16,62 @@ public class Jeu {
      */
     public static void afficherTableau(int[][] tableau, int axe_x, int axe_y, int compte_x, int compte_y) {
 
+        // Affichag de la barre verticale à la première cellule
         if ((compte_x == 1) && (compte_y == 1))
         {
             System.out.print(" |");
         }
 
-        if ( compte_x != axe_x - 1)
+        // Affichage de la valeur de la cellule suivi d'une barre verticale
+        if (compte_x != axe_x - 1)
         {
             System.out.print(tableau[compte_y][compte_x] + "|" );
+            // Passage à la celulle suivante sur la même ligne
             afficherTableau(tableau, axe_x,axe_y,compte_x +1 ,compte_y);
         }
 
-        if ((  compte_x == axe_x - 1 ) && (compte_y != axe_y - 2))
+        // Passage à la ligne suivante après la dernière colonne de la ligne
+        if ((compte_x == axe_x - 1) && (compte_y != axe_y - 2))
         {
             System.out.print("\n |");
+            // Passage à la première colonne de la ligne suivante
             afficherTableau(tableau, axe_x,axe_y,1 , compte_y +1);
         }
     }
 
     /**
-     *
+     * Initialisation du tableau au lancement de la partie
      * @param tableau: le tableau qui contient les infos que l'on veut afficher
      * @param compte_x: pointeur selon l'axe horizontale qui va nous permettre de nous repérer sur le plateau
      * @param compte_y: pointeur selon l'axe verticale qui va nous permettre de nous repérer sur le plateau
      */
     public static void init_tableau(int[][] tableau, int compte_x, int compte_y)
     {
+        // Vérifie si la position actuelle se trouve aux bords du tableau
         if (((compte_y == 0)||(compte_y == 3)||(compte_x == 0)||(compte_x == 4)))
         {
+            // Si elle se trouve sur le bord, on défini
+            // la valeur à cette position une ligne au dessus
             tableau[compte_y][compte_x] = -1;
+
+            // Fait un déplacement à la colonne suivante (si existante.s)
+            // selon la position actuelle
             if (compte_x<4)
             {
                 tableau[compte_y][compte_x] = 0;
                 init_tableau(tableau,compte_x+1,compte_y);
             }
 
+            // Si la position actuelle est sur le bprd, le déplacement est effectué
+            // à la première colonne de la ligne suivante
             else if (compte_y < 3)
             {
                 init_tableau(tableau,0,compte_y+1);
             }
         }
+
+        // Si la position actuelle n'est pas sur le bord,
+        // on lui assigne la valeur 0 et on la déplace à la colonne suivante
         else
         {
             tableau[compte_y][compte_x] = 0;
